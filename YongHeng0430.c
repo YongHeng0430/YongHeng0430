9c758dee485353044ce997cce1158cd7987af76a
YongHeng0430 <141827326+YongHeng0430@users.noreply.github.com>
2025-08-07 20:01:12 +0800

    Update YongHeng0430.c

--- YongHeng0430.c
+++ YongHeng0430.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 //程序入口
 int main(){
-  //printf代表输出，小括号代表的是输出内容
-printf('HelloWorld')
+  //printf代表输出，小括号代表的是输出内容，;代表句号
+printf('HelloWorld');
 
 
-printf(“我是人类已是:%d岁”;, 999)
+printf(“我是人类已是:%d岁”;, 999);
   
 
-printf(“你们好啊，我是are you ok？”)
+printf(“你们好啊，我是are you ok？”);
   
 
-printf(“我是ait:%d岁”, 999)
+printf(“我是ait:%d岁”, 999);
   
 
-printf('我是YongHeng0430大帝')
+printf('我是YongHeng0430大帝');
 
 
 
