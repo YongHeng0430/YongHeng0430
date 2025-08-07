fd250aeae01ca70229115d20723a551c3b74398e
YongHeng0430 <141827326+YongHeng0430@users.noreply.github.com>
2025-08-07 19:10:47 +0800

    Update YongHeng0430.c

--- YongHeng0430.c
+++ YongHeng0430.c
@@ -6,23 +6,16 @@ int main(){
 printf('HelloWorld')
 
 
-printf(“我是人类已是:%d岁”;,9999999)
+printf(“我是人类已是:%d岁”;, 999)
   
 
 printf(“你们好啊，我是are you ok？”)
   
 
-printf(“我是ait:%d岁”,999999999999)
+printf(“我是ait:%d岁”, 999)
   
 
-printf('我是YongHeng0430大帝😡')
-
-
-
-
-
-
-
+printf('我是YongHeng0430大帝')
 
 
 
