554838ae7eb3fa857f3798480b044615eb66882e
YongHeng0430 <141827326+YongHeng0430@users.noreply.github.com>
2025-08-07 20:20:06 +0800

    Update YongHeng0430.c

--- YongHeng0430.c
+++ YongHeng0430.c
@@ -3,19 +3,19 @@
 //程序入口
 int main(){
   //printf代表输出，小括号代表的是输出内容，;代表句号
-printf("HelloWorld");
+printf("HelloWorld\n");
 
 
-printf("我是人类已是:%d岁", 999);
+printf("我是人类已是:%d岁\n", 999);
   
 
-printf("你们好啊，我是are you ok？");
+printf("你们好啊，我是are you ok？\n");
   
 
-printf("我是ait:%d岁", 999);
+printf("我是ait:%d岁\n", 999);
   
 
-printf("我是YongHeng0430大帝");
+printf("我是YongHeng0430大帝\n");
 
 
 //程序运行结果，0代表正常运行
